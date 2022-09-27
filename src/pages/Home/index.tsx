dac8e7dbbd34303b693acefbfdcf87895769f625
Rafa Martins <rafamartins.dev@gmail.com>
2022-09-27 10:15:05 -0300

    use Hero component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { Hero } from './components/Hero'
+import { HomeContainer } from './styles'
+
 export function HomePage() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <HomeContainer>
+      <Hero />
+    </HomeContainer>
   )
 }
