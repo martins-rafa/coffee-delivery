f749151c08cda1f2fe092f754bc689fe9a9cfd95
Rafa Martins <rafamartins.dev@gmail.com>
2022-09-28 10:33:59 -0300

    add Products section to the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Hero } from './components/Hero'
+import { Products } from './components/Products'
 import { HomeContainer } from './styles'
 
 export function HomePage() {
   return (
     <HomeContainer>
       <Hero />
+      <Products />
     </HomeContainer>
   )
 }
