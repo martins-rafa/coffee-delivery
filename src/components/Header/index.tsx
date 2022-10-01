30f06cf6503a3b61108b8bbfb4bbe87e41ea3cfe
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-01 12:09:16 -0300

    add page navigation to header buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,15 @@ import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 
 import Logo from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <div className="container">
-        <img src={Logo} alt="" />
+        <NavLink to="/">
+          <img src={Logo} alt="" />
+        </NavLink>
 
         <HeaderButtonsContainer>
           <HeaderButton className="purple">
@@ -16,9 +19,11 @@ export function Header() {
             Rio de Janeiro, RJ
           </HeaderButton>
 
-          <HeaderButton className="yellow">
-            <ShoppingCart size={20} weight="fill" />
-          </HeaderButton>
+          <NavLink to="/checkout">
+            <HeaderButton className="yellow">
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
