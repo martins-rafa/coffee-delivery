db29b26e289a7b88179de398019b79375409bfe8
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-02 10:39:40 -0300

    Order Successfully done Page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,62 @@
+import { Paragraph, Title } from '../../components/Typography'
+import { DetailsContainer, SuccessPageContainer } from './styles'
+
+import SuccessIllustration from '../../assets/Success-Illustration.png'
+import { Info } from '../../components/Info'
+import { Clock, CurrencyDollar, MapPin } from 'phosphor-react'
+import { useTheme } from 'styled-components'
+
 export function SuccessPage() {
+  const { colors } = useTheme()
   return (
-    <>
-      <h1>Success</h1>
-    </>
+    <SuccessPageContainer className="container">
+      <div>
+        <Title size="l">Uhu! Order confirmed</Title>
+        <Paragraph size="l" color="subtitle">
+          Your order will arrive in a few minutes
+        </Paragraph>
+      </div>
+
+      <section>
+        <DetailsContainer>
+          <Info
+            icon={<MapPin weight="fill" />}
+            iconBg={colors['brand-purple']}
+            text={
+              <Paragraph>
+                Delivery at <strong>Example Street, 96</strong>
+                <br />
+                Copacabana - Rio de Janeiro, RJ
+              </Paragraph>
+            }
+          />
+
+          <Info
+            icon={<Clock weight="fill" />}
+            iconBg={colors['brand-yellow']}
+            text={
+              <Paragraph>
+                Arrives in
+                <br />
+                <strong>20 - 30 minutes</strong>
+              </Paragraph>
+            }
+          />
+
+          <Info
+            icon={<CurrencyDollar weight="fill" />}
+            iconBg={colors['brand-yellow-dark']}
+            text={
+              <Paragraph>
+                Payment method
+                <br />
+                <strong>Credit Card</strong>
+              </Paragraph>
+            }
+          />
+        </DetailsContainer>
+        <img src={SuccessIllustration} alt="" />
+      </section>
+    </SuccessPageContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,48 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const SuccessPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  margin-top: 5rem;
+
+  h1 {
+    color: ${({ theme }) => theme.colors['brand-yellow-dark']};
+  }
+
+  > section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 1020px) {
+      flex-direction: column;
+    }
+  }
+`
+
+export const DetailsContainer = styled.div`
+  min-width: 32rem;
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  border-radius: 6px 36px 6px 36px;
+
+  background: linear-gradient(
+        ${(props) => props.theme.colors['base-background']},
+        ${(props) => props.theme.colors['base-background']}
+      )
+      padding-box,
+    linear-gradient(
+        120deg,
+        ${(props) => props.theme.colors['brand-yellow']},
+        ${(props) => props.theme.colors['brand-purple']}
+      )
+      border-box;
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
+
+  > div:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`
