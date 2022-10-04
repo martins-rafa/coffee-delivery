a502125031b2eee7d9085028a48b2eb1c29b66ab
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-04 08:59:04 -0300

    change input component to receive error messages if the user dont enter required information

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,19 @@
-import { InputHTMLAttributes } from 'react'
-import { InputStyleContainer } from './styles'
+import { forwardRef, InputHTMLAttributes } from 'react'
+import { Paragraph } from '../Typography'
+import { InputContainer, InputStyleContainer } from './styles'
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
-
-export function Input({ ...props }: InputProps) {
-  return <InputStyleContainer {...props} />
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string
 }
+
+// eslint-disable-next-line react/display-name
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, ...props }, ref) => {
+    return (
+      <InputContainer className={className}>
+        <InputStyleContainer {...props} ref={ref} />
+        {error && <Paragraph size="s">{error}</Paragraph>}
+      </InputContainer>
+    )
+  },
+)
